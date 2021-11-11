f8c3d2f459492cc8bcc1435d1f2780d39681e46c
Callymags <callanmaguire18@gmail.com>
2021-11-11 11:23:32 +0000

    Finished product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -10,15 +10,31 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items(): 
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id, 
-            'quantity': quantity, 
-            'product': product,
-        })
+    for item_id, item_data in bag.items(): 
+        # Only execute if item has no sizes by checking if its an integer only
+        # If item has sizes it would be converted into a dictionary
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id, 
+                'quantity': item_data, 
+                'product': product,
+            })
+        # Iterates through items_by_size dictionary and increments the 
+        # product count and total 
+        else: 
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id, 
+                    'quantity': item_data, 
+                    'product': product,
+                    'size': size, 
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- bag/views.py
+++ bag/views.py
@@ -13,13 +13,30 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None 
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
-    # Increase quantity of item in bag if item id is already in bag
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
-    else:     
-        bag[item_id] = quantity
+    # If item has size
+    if size:
+        if item_id in list(bag.keys()):
+            # Checks if there is another item with same id and size 
+            if size in bag[item_id]['items_by_size'].keys():
+                # If so, increment quantity of size
+                bag[item_id]['item_by_size'][size] += quantity
+            else: 
+                # If not, add new item with size to basket
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    # If item doesn't have a size
+    else: 
+        # Increase quantity of item in bag if item id is already in bag
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:     
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
