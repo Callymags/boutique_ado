743867d1c9659d65d90d7d44e8b04209869b5f11
Callymags <callanmaguire18@gmail.com>
2021-11-21 18:28:12 +0000

    Add custom checkout to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInline(admin.TabularInline):
+    # When we look at order, we can see list of editable line items on same page
+    # rather than having to go to the OrderLineItem interface
+    model = OrderLineItem
+    # Make the line_item total read only 
+    read_only_fields = ('lineitem_total',)
+
+
+
+class OrderAdmin(admin.ModelAdmin):
+    # Add lineitem total to Order Admin interface 
+    inlines = (OrderLineItemAdminInline,)
+    # Users will not be able to input info in these fields as these settings are 
+    # calculated by the methods in models.py
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+    # Allows us to specify the order of the field in the admin interface
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country', 
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county', 'delivery_cost',
+              'order_total', 'grand_total',)
+
+    # Restrict the columns that show up in the order list to only a few key items
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost', 'grand_total',)
+
+    # Set the most recent orders to the top
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
--- checkout/apps.py
+++ checkout/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'checkout'
+
+    # Override ready method and import signals module
+    def ready(self):
+        import checkout.signals
